d48f9b856041059a28effd8a5216cf8395dc065a
Hephaestus0x <0xober@proton.me>
2024-04-14 23:26:06 +0200

    Fixed hardhat config for CI

--- hardhat.configCI.ts
+++ hardhat.configCI.ts
@@ -1,37 +1,49 @@
 import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomiclabs/hardhat-web3";
-import '@openzeppelin/hardhat-upgrades';
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
 
-console.log('🚨 Change the env file in production 🚨');
-dotenv.config({path: process.cwd() + '/scripts/processCI.env'});
+dotenv.config({path: process.cwd() + '/processCI.env'});
+
+const testAccounts = [
+  {
+    privateKey: process.env.ADMIN_WALLET_KEY!,
+    balance: "1000000000000000000000000000000000000000000",
+  },
+  {
+    privateKey: process.env.TEAM_WALLET_KEY!,
+    balance: "1000000000000000000000000000000000000000000",
+  },
+  {
+    privateKey: process.env.USER_A_WALLET_KEY!,
+    balance: "1000000000000000000000000000000000000000000",
+  },
+  {
+    privateKey: process.env.USER_B_WALLET_KEY!,
+    balance: "1000000000000000000000000000000000000000000",
+  },
+  {
+    privateKey: process.env.USER_C_WALLET_KEY!,
+    balance: "1000000000000000000000000000000000000000000",
+  },
+];
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.20",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
-      },
-      {
-        version: "0.8.18",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 100
-          }
-        }
-      },
+            runs: 999999,
+          },
+        },
+      }
     ]
-
   },
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "localhost",
   networks: {
     // Configure each network to the respective Cronos instances
     hardhat: {
@@ -40,63 +52,33 @@ const config: HardhatUserConfig = {
       //  enabled: true,
       //  blockNumber: 5168517,
       //},
-      accounts: [
-        {
-          privateKey: process.env.AEGIS_MANAGER_KEY!, //same as strat owner
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.AEGIS_TREASURY_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.USER_A_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.USER_B_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.USER_C_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.USER_D_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        },
-        {
-          privateKey: process.env.USER_E_KEY!,
-          balance: '1000000000000000000000000000000000000000000'
-        }
-      ],
+      accounts: testAccounts,
     },
-    cronosMainnet: {
-      url: 'https://evm.cronos.org',
-      chainId: 25,
-      accounts: [process.env.AEGIS_MANAGER_KEY!],
-      gas: 'auto',
-      gasPrice: 'auto',
-      allowUnlimitedContractSize: true
-    },
-    cronosTestnet: {
-      url: 'https://evm-t3.cronos.org',
-      chainId: 338,
-      accounts: [process.env.AEGIS_MANAGER_KEY!],
-      gas: 'auto',
-      gasPrice: 'auto',
-      allowUnlimitedContractSize: true
-    },
-    avaxTestnet: {
-      url: 'https://api.avax-test.network/ext/bc/C/rpc',
-      chainId: 43113,
-      accounts: [process.env.AEGIS_MANAGER_KEY!],
-      gas: 'auto',
-      gasPrice: 'auto',
-      allowUnlimitedContractSize: true
-    },
+    //cronosMainnet: {
+    //  url: "https://evm.cronos.org",
+    //  chainId: 25,
+    //  accounts: [process.env.AEGIS_MANAGER_KEY!],
+    //  gas: "auto",
+    //  gasPrice: "auto",
+    //  allowUnlimitedContractSize: true,
+    //},
+    //cronosTestnet: {
+    //  url: "https://evm-t3.cronos.org",
+    //  chainId: 338,
+    //  accounts: [process.env.AEGIS_MANAGER_KEY!],
+    //  gas: "auto",
+    //  gasPrice: "auto",
+    //  allowUnlimitedContractSize: true,
+    //},
+    //avaxTestnet: {
+    //  url: "https://api.avax-test.network/ext/bc/C/rpc",
+    //  chainId: 43113,
+    //  accounts: [process.env.AEGIS_MANAGER_KEY!],
+    //  gas: "auto",
+    //  gasPrice: "auto",
+    //  allowUnlimitedContractSize: true,
+    //},
   },
 };
 
 export default config;
-
