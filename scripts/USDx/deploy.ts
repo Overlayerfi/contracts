c3dc49a67d6968fa01d0e5876fd776c583fcbae5
Hephaestus0x <0xober@proton.me>
2024-04-22 11:03:38 +0200

    Script reorganization / local batch deployment script

--- scripts/USDx/deploy.ts
+++ scripts/USDx/deploy.ts
@@ -1,37 +1,5 @@
-import { ethers } from "hardhat";
-import { USDC_ADDRESS, USDT_ADDRESS } from "../addresses";
+import { deployUSDx } from "../functions";
 
-export async function deploy(
-): Promise<void> {
-  const [deployer] = await ethers.getSigners();
-
-  console.log(
-    'Deploying USDxM contract with address:',
-    deployer.address
-  );
-
-  const ContractSource = await ethers.getContractFactory(
-    'USDxM'
-  );
-  const deployedContract = await ContractSource.deploy(
-		await deployer.getAddress(),
-		{
-			addr: USDC_ADDRESS,
-			decimals: 6
-		},
-		{
-			addr: USDT_ADDRESS,
-			decimals: 6
-		},
-		await deployer.getAddress(),
-		ethers.parseEther('100000000'),
-		ethers.parseEther('100000000')
-  );
-  await deployedContract.waitForDeployment();
-
-  console.log('Contract deployed at:', await deployedContract.getAddress());
-}
-
-deploy().catch((err) => {
+deployUSDx().catch((err) => {
 	console.error("Deployment failed -> " + err);
 })
--- scripts/liquidity/deploy.ts
+++ scripts/liquidity/deploy.ts
@@ -1,26 +1,5 @@
-import { ethers } from "hardhat";
+import { deployLiquidity } from "../functions";
 
-export async function deploy(
-  devAddress: string,
-  startingBlock: number
-): Promise<void> {
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    'Deploying Liquidity contract with address:',
-    deployer.address
-  );
-
-  const ContractSource = await ethers.getContractFactory('Liquidity');
-  const deployedContract = await ContractSource.deploy(
-    devAddress,
-    startingBlock
-  );
-
-  await deployedContract.waitForDeployment();
-
-  console.log('Contract deployed at:', await deployedContract.getAddress());
-}
-
-deploy('', 0).catch((error) => {
+deployLiquidity('', 0).catch((error) => {
   console.error("Deployment failed ->", error);
 });
