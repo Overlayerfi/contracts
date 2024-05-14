af4b2a35775b1ba8587ead23fae1a1f4aed0e8f7
Hephaestus0x <0xober@proton.me>
2024-05-15 00:12:24 +0200

    Added util script

---
+++ scripts/utils/sendERC20.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+import {Contract} from "ethers";
+import { USDC_ABI } from "../get_stables_from_uniswap_local/USDC_abi";
+
+export async function transfer(token: string, decimals: number, dest: string, amount: string): Promise<void> {
+  const [admin] = await ethers.getSigners();
+  const contract = new ethers.Contract(token, USDC_ABI, admin);
+  await (contract.connect(admin) as Contract).transfer(dest, ethers.parseUnits(amount, decimals));
+}
+
+transfer("", 6, "", "10")
+  .catch((error) => {
+    console.error(error);
+  })
+  .then((res) => {
+    console.log("Transfer completed");
+  });
