bc3a4b21dcdd9b18ba7fcf9ef2e3f2ffeb2843b9
Hephaestus0x <0xober@proton.me>
2024-05-14 21:22:50 +0200

    Formatting

--- scripts/StakedUSDO/setCooldown.ts
+++ scripts/StakedUSDO/setCooldown.ts
@@ -1,7 +1,8 @@
 import { StakedUSDO_setCooldownStaking } from "../functions";
 
-StakedUSDO_setCooldownStaking("0x9E7ef64F17E79366e70C1Fdc01E1A00323e1FCF8", 60).catch(
-  (err) => {
-    console.error("Operation failed -> " + err);
-  }
-);
+StakedUSDO_setCooldownStaking(
+  "0x9E7ef64F17E79366e70C1Fdc01E1A00323e1FCF8",
+  60
+).catch((err) => {
+  console.error("Operation failed -> " + err);
+});
