a54c41bff8b75326a6ce1bab6d78cd4435d144f3
Hephaestus0x <0xober@proton.me>
2024-10-05 00:24:27 +0200

    Prettier

--- scripts/get_stables_from_uniswap_local/swap.ts
+++ scripts/get_stables_from_uniswap_local/swap.ts
@@ -1,7 +1,12 @@
 import { ethers } from "hardhat";
 import { Contract } from "ethers";
 import { WETH_ABI } from "../abi/WETH_abi";
-import { DAI_ADDRESS, USDC_ADDRESS, USDT_ADDRESS, WETH_MAINNET_ADDRESS } from "../addresses";
+import {
+  DAI_ADDRESS,
+  USDC_ADDRESS,
+  USDT_ADDRESS,
+  WETH_MAINNET_ADDRESS
+} from "../addresses";
 import { USDC_ABI } from "../abi/USDC_abi";
 import { USDT_ABI } from "../abi/USDT_abi";
 import { DAI_ABI } from "../abi/DAI_abi";
@@ -11,7 +16,11 @@ let SWAP_CODES = [1, 2];
 // code 0: DAI
 // code 1: USDC
 // code 2: USDT
-export async function swap(wethAmountToWrap: string, wethAmountToSwap: string, code?: number) {
+export async function swap(
+  wethAmountToWrap: string,
+  wethAmountToSwap: string,
+  code?: number
+) {
   const [deployer] = await ethers.getSigners();
   console.log(
     "Deploying UniswapV3SingleHopSwap contract with signer:",
@@ -23,7 +32,7 @@ export async function swap(wethAmountToWrap: string, wethAmountToSwap: string, c
   const baseFee = block.baseFeePerGas;
   const maxFee = baseFee * BigInt(10);
   const defaultTransactionOptions = {
-    maxFeePerGas: maxFee 
+    maxFeePerGas: maxFee
   };
 
   const swapContract = await ethers.getContractFactory(
@@ -50,7 +59,7 @@ export async function swap(wethAmountToWrap: string, wethAmountToSwap: string, c
   );
   console.log("Spender approved");
 
-  SWAP_CODES = code === undefined ? SWAP_CODES : [code]; 
+  SWAP_CODES = code === undefined ? SWAP_CODES : [code];
   for (let i = 0; i < SWAP_CODES.length; ++i) {
     await swapper
       .connect(deployer)
@@ -65,11 +74,18 @@ export async function swap(wethAmountToWrap: string, wethAmountToSwap: string, c
   const usdcContract = new ethers.Contract(USDC_ADDRESS, USDC_ABI, deployer);
   const usdtContract = new ethers.Contract(USDT_ADDRESS, USDT_ABI, deployer);
   const daiContract = new ethers.Contract(DAI_ADDRESS, DAI_ABI, deployer);
-  for (const t of [{"contract": usdcContract, "name": "usdc", "decimals": 6}, {"contract": usdtContract, "name": "usdt", "decimals": 6}, {"contract": daiContract, "name": "dai", "decimals": 18}]) {
+  for (const t of [
+    { contract: usdcContract, name: "usdc", decimals: 6 },
+    { contract: usdtContract, name: "usdt", decimals: 6 },
+    { contract: daiContract, name: "dai", decimals: 18 }
+  ]) {
     console.log(
       deployer.address,
       `${t.name} balance`,
-      ethers.formatUnits(await t.contract.balanceOf(deployer.address), t.decimals)
+      ethers.formatUnits(
+        await t.contract.balanceOf(deployer.address),
+        t.decimals
+      )
     );
   }
 }
