0b80109ed529ee8e6c3ba414435e34011b656ad6
Hephaestus0x <0xober@proton.me>
2024-12-17 16:10:33 +0100

    Forked v3 staker

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,6 +42,18 @@ const config: HardhatUserConfig = {
             runs: 999999,
           },
         },
+      },
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1_000_000,
+          },
+          metadata: {
+            bytecodeHash: 'none',
+          },
+        },
       }
     ]
   },
