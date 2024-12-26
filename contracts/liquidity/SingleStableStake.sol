0584314d5d1b0be8e23032cc106db669d9556d9a
Hephaestus0x <0xober@proton.me>
2024-12-26 16:50:04 +0100

    Airdrop: single stable stake

---
+++ contracts/liquidity/SingleStableStake.sol
@@ -0,0 +1,171 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+import "./interfaces/ILiquidityDefs.sol";
+import "./interfaces/IRewardAsset.sol";
+import "./Liquidity.sol";
+
+/**
+ * @notice Single stable coin staking contract implementation.
+ * This contract supports multiple staking pools to be deployed in the same contract sharing the reward token.
+ */
+contract SingleStableStake is Liquidity {
+    using SafeERC20 for IERC20;
+    using Math for uint256;
+
+    /**
+     * @notice The emitted amount of reward for each second multiplier numerator.
+     */
+    mapping(address => uint256) public rewardsPerSecondMultiplierNum;
+
+    /**
+     * @notice The emitted amount of reward for each second multiplier denominator.
+     */
+    mapping(address => uint256) public rewardsPerSecondMultiplierDen;
+
+    /**
+     * @notice Contract constructor.
+     * @param admin The contract admin
+     * @param startTime_ The reward start time
+     */
+    constructor(
+        address admin,
+        uint256 startTime_
+    ) Liquidity(admin, startTime_) {}
+
+    /**
+     * @notice Set a reward rate.
+     * @param rewardAsset the reward.
+     * @param rewardRateNum the new reward rate numerator.
+     * @param rewardRateDen the new reward rate denominator.
+     */
+    function setRewardForStakedAssets(
+        IERC20 rewardAsset,
+        uint256 rewardRateNum,
+        uint256 rewardRateDen
+    ) external onlyOwner {
+        if (!activeRewards[address(rewardAsset)]) {
+            activeRewards[address(rewardAsset)] = true;
+        }
+        if (
+            rewardsPerSecondMultiplierNum[address(rewardAsset)] != rewardRateNum
+        ) {
+            rewardsPerSecondMultiplierNum[address(rewardAsset)] = rewardRateNum;
+        }
+        if (
+            rewardsPerSecondMultiplierDen[address(rewardAsset)] != rewardRateDen
+        ) {
+            rewardsPerSecondMultiplierDen[address(rewardAsset)] = rewardRateDen;
+        }
+    }
+
+    /**
+     * @notice Get the pending reward for a given pool and user.
+     * @param pid the pool identifier.
+     * @param user the participant.
+     * @return the pending reward for given pool and user.
+     */
+    function pendingReward(
+        uint256 pid,
+        address user
+    ) public view override returns (uint256) {
+        if (pid >= poolInfo.length) {
+            revert InvalidPid();
+        }
+
+        PoolInfo memory pool = poolInfo[pid];
+        UserInfo memory currentUser = userInfo[pid][user];
+
+        uint256 accRewardPerShare = pool.accRewardPerShare;
+        uint256 stakedAssetSupply = pool.stakedAsset.balanceOf(address(this));
+        if (block.timestamp > pool.lastRewardTime && stakedAssetSupply != 0) {
+            uint256 multiplier = _getMultiplier(
+                pool.lastRewardTime,
+                block.timestamp
+            );
+
+            // This is the same computation made in the updatePool function. Just a view version.
+            uint256 rewards = multiplier *
+                (
+                    rewardsForStakedAssets(pool.stakedAsset, pool.rewardAsset)
+                        .mulDiv(
+                            pool.allocPoints,
+                            totalAllocPointsPerReward[address(pool.rewardAsset)]
+                        )
+                );
+            accRewardPerShare =
+                accRewardPerShare +
+                rewards.mulDiv(1e18, stakedAssetSupply);
+
+            return
+                currentUser.amount.mulDiv(accRewardPerShare, 1e18) -
+                currentUser.rewardDebt;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @notice Update pool infos.
+     * @param pid the pool identifier.
+     */
+    function updatePool(uint256 pid) internal override {
+        PoolInfo storage pool = poolInfo[pid];
+        if (block.timestamp <= pool.lastRewardTime) {
+            return;
+        }
+        uint256 stakedAssetSupply = pool.stakedAsset.balanceOf(address(this));
+        if (stakedAssetSupply == 0) {
+            pool.lastRewardTime = block.timestamp;
+            return;
+        }
+        uint256 multiplier = _getMultiplier(
+            pool.lastRewardTime,
+            block.timestamp
+        );
+        uint256 rewards = multiplier *
+            (
+                rewardsForStakedAssets(pool.stakedAsset, pool.rewardAsset)
+                    .mulDiv(
+                        pool.allocPoints,
+                        totalAllocPointsPerReward[address(pool.rewardAsset)]
+                    )
+            );
+        pool.accRewardPerShare =
+            pool.accRewardPerShare +
+            rewards.mulDiv(1e18, stakedAssetSupply);
+
+        pool.lastRewardTime = block.timestamp;
+    }
+
+    /**
+     * @notice Get the reward amount for second based on the total staked liquidity.
+     * @dev If the staked amount is less than 1 ether or less than a second then the emitted amount will be zero.
+     * @param staked the staked asset.
+     * @param reward the reward asset.
+     * @return the emitted reward for staked asset for second.
+     */
+    function rewardsForStakedAssets(
+        IERC20 staked,
+        IERC20 reward
+    ) internal view returns (uint256) {
+        uint256 secondsInYear = 1 days * 365;
+        uint256 stakedAmount = staked.balanceOf(address(this));
+
+        // No liquidity
+        if (stakedAmount < 1 ether) {
+            return 0;
+        }
+
+        return
+            stakedAmount.mulDiv(
+                rewardsPerSecondMultiplierNum[address(reward)],
+                rewardsPerSecondMultiplierDen[address(reward)]
+            ) / secondsInYear;
+    }
+}
---
+++ test/SingleStableStake.ts
@@ -0,0 +1,491 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { ethers } from "hardhat";
+import { expect, assert } from "chai";
+
+// This contract shares the basic staking rewards math with `Liquidity.sol` and hence some tests are being ignored as tested within the other test
+describe("SingleStableStake", function () {
+  async function deployFixture() {
+    const latestTime: number = await time.latest();
+    const [owner, notOwner, alice, bob] = await ethers.getSigners();
+
+    // For localhost
+    const block = await owner.provider.getBlock("latest");
+    const baseFee = block.baseFeePerGas;
+    const defaultTransactionOptions = {
+      maxFeePerGas: baseFee * BigInt(10)
+    };
+
+    const Liquidity = await ethers.getContractFactory("SingleStableStake");
+    const liquidity = await Liquidity.deploy(
+      owner.getAddress(),
+      latestTime,
+      defaultTransactionOptions
+    );
+
+    const StakedAsset = await ethers.getContractFactory("TokenLP_A_B");
+    const stakedAsset = await StakedAsset.deploy(
+      ethers.parseEther("1000"),
+      "LPABONE",
+      "LPABONE",
+      defaultTransactionOptions
+    );
+    await stakedAsset.setMinter(liquidity.getAddress());
+
+    const TokenRewardOne = await ethers.getContractFactory("OvaReferral");
+    const tokenRewardOneOvaReferral = await TokenRewardOne.deploy(
+      owner.address,
+      defaultTransactionOptions
+    );
+    await tokenRewardOneOvaReferral.setMinter(liquidity.getAddress());
+    // not using OvaReferral as in some tests we need to transfer it
+    const TokenRewardTwo = await ethers.getContractFactory("TokenLP_A_B");
+    const tokenRewardTwo = await TokenRewardTwo.deploy(
+      ethers.parseEther("1000"),
+      "LPABTWO",
+      "LPABTWO",
+      defaultTransactionOptions
+    );
+    await tokenRewardTwo.setMinter(liquidity.getAddress());
+
+    return {
+      liquidity,
+      stakedAsset,
+      tokenRewardOneOvaReferral,
+      tokenRewardTwo,
+      latestTime,
+      owner,
+      notOwner,
+      alice,
+      bob
+    };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner address", async function () {
+      const { liquidity, owner } = await loadFixture(deployFixture);
+      expect(await liquidity.owner()).to.equal(await owner.getAddress());
+    });
+
+    it("Should set the right start timestamp", async function () {
+      const { liquidity, latestTime } = await loadFixture(deployFixture);
+      expect(await liquidity.startTime()).to.equal(latestTime);
+    });
+  });
+
+  describe("ModifyParam", function () {
+    it("Should update multiplier", async function () {
+      const { liquidity, owner } = await loadFixture(deployFixture);
+      await liquidity.connect(owner).updateMultiplier(2);
+      expect(await liquidity.bonusMultiplier()).to.equal(2);
+    });
+
+    it("Should revert update multiplier if not owner", async function () {
+      const { liquidity, notOwner } = await loadFixture(deployFixture);
+      await expect(liquidity.connect(notOwner).updateMultiplier(2)).to.be
+        .eventually.rejected;
+    });
+
+    it("Should update starting timestamp", async function () {
+      const { liquidity, owner, latestTime } = await loadFixture(deployFixture);
+      await liquidity.connect(owner).updateStartTime(latestTime + 100);
+      expect(await liquidity.startTime()).to.equal(latestTime + 100);
+    });
+
+    it("Should revert update multiplier if not owner", async function () {
+      const { liquidity, notOwner } = await loadFixture(deployFixture);
+      await expect(liquidity.connect(notOwner).updateStartTime(2)).to.be
+        .eventually.rejected;
+    });
+  });
+
+  describe("GetInfo", function () {
+    it("Should get pool length", async function () {
+      const { liquidity } = await loadFixture(deployFixture);
+      expect(await liquidity.poolLength()).to.equal(0);
+    });
+  });
+
+  describe("AddPool", function () {
+    it("Should not add a new pool if start timestamp for rewards is zero", async function () {
+      const { liquidity, stakedAsset, owner, tokenRewardOneOvaReferral } =
+        await loadFixture(deployFixture);
+      await liquidity.connect(owner).updateStartTime(0);
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await expect(
+        liquidity.add(
+          stakedAsset.getAddress(),
+          tokenRewardOneOvaReferral.getAddress(),
+          1,
+          true
+        )
+      ).to.be.eventually.rejected;
+    });
+
+    it("Should add a new pool", async function () {
+      const { liquidity, stakedAsset, tokenRewardOneOvaReferral } =
+        await loadFixture(deployFixture);
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await liquidity.add(
+        stakedAsset.getAddress(),
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        true
+      );
+      expect(await liquidity.poolLength()).to.equal(1);
+    });
+
+    it("Should return correct allocation points for different pools", async function () {
+      const {
+        liquidity,
+        stakedAsset,
+        tokenRewardOneOvaReferral,
+        tokenRewardTwo
+      } = await loadFixture(deployFixture);
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await liquidity.add(
+        stakedAsset.getAddress(),
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        true
+      );
+      expect(await liquidity.poolLength()).to.equal(1);
+      await liquidity.setRewardForStakedAssets(stakedAsset.getAddress(), 1, 1);
+      await liquidity.add(
+        tokenRewardOneOvaReferral.getAddress(),
+        stakedAsset.getAddress(),
+        10,
+        true
+      );
+      expect(await liquidity.poolLength()).to.equal(2);
+      await liquidity.add(
+        tokenRewardTwo.getAddress(),
+        stakedAsset.getAddress(),
+        100,
+        true
+      );
+      expect(await liquidity.poolLength()).to.equal(3);
+      expect(
+        await liquidity.totalAllocPointsPerReward(
+          tokenRewardOneOvaReferral.getAddress()
+        )
+      ).to.be.equal(1);
+      expect(
+        await liquidity.totalAllocPointsPerReward(stakedAsset.getAddress())
+      ).to.be.equal(110);
+    });
+
+    it("Should not add a new pool if not owner", async function () {
+      const { liquidity, stakedAsset, tokenRewardOneOvaReferral, notOwner } =
+        await loadFixture(deployFixture);
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await expect(
+        liquidity
+          .connect(notOwner)
+          .add(
+            stakedAsset.getAddress(),
+            tokenRewardOneOvaReferral.getAddress(),
+            1,
+            false
+          )
+      ).to.be.eventually.rejected;
+    });
+  });
+
+  describe("SetReward", function () {
+    it("Should set reward", async function () {
+      const { liquidity, stakedAsset } = await loadFixture(deployFixture);
+      await liquidity.setRewardForStakedAssets(stakedAsset.getAddress(), 1, 1);
+      expect(
+        await liquidity.activeRewards(stakedAsset.getAddress())
+      ).to.be.equal(true);
+    });
+
+    it("Should change reward rate", async function () {
+      const { liquidity, stakedAsset } = await loadFixture(deployFixture);
+      await liquidity.setRewardForStakedAssets(stakedAsset.getAddress(), 1, 1);
+      expect(
+        await liquidity.activeRewards(stakedAsset.getAddress())
+      ).to.be.equal(true);
+      expect(
+        await liquidity.rewardsPerSecondMultiplierNum(stakedAsset.getAddress())
+      ).to.be.equal(1);
+      await liquidity.setRewardForStakedAssets(stakedAsset.getAddress(), 10, 1);
+      expect(
+        await liquidity.rewardsPerSecondMultiplierNum(stakedAsset.getAddress())
+      ).to.be.equal(10);
+    });
+
+    it("Should not set reward", async function () {
+      const { liquidity, stakedAsset, notOwner } = await loadFixture(
+        deployFixture
+      );
+      await expect(
+        liquidity
+          .connect(notOwner)
+          .setRewardForStakedAssets(stakedAsset.getAddress(), 1, 1)
+      ).to.be.eventually.rejected;
+    });
+  });
+
+  describe("CoreFunctionality", function () {
+    it("Deposit", async function () {
+      const { liquidity, stakedAsset, tokenRewardOneOvaReferral, alice, bob } =
+        await loadFixture(deployFixture);
+
+      const latestTime: number = await time.latest();
+      await time.increaseTo(latestTime + 1);
+
+      await stakedAsset.transfer(alice.getAddress(), 10);
+      await stakedAsset.transfer(bob.getAddress(), 10);
+
+      await stakedAsset.connect(alice).approve(liquidity.getAddress(), 10);
+      await stakedAsset.connect(bob).approve(liquidity.getAddress(), 10);
+
+      expect(
+        await stakedAsset.allowance(alice.getAddress(), liquidity.getAddress())
+      ).to.equal(10);
+      expect(
+        await stakedAsset.allowance(bob.getAddress(), liquidity.getAddress())
+      ).to.equal(10);
+
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await liquidity.add(
+        stakedAsset.getAddress(),
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        true
+      );
+
+      await expect(await liquidity.connect(alice).deposit(0, 5)).to.emit(
+        liquidity,
+        "Deposit"
+      );
+      await expect(await liquidity.connect(bob).deposit(0, 5)).to.emit(
+        liquidity,
+        "Deposit"
+      );
+    });
+
+    it("Rewards for staked liquidity", async function () {
+      const {
+        owner,
+        liquidity,
+        stakedAsset,
+        tokenRewardOneOvaReferral,
+        alice
+      } = await loadFixture(deployFixture);
+
+      await stakedAsset.transfer(alice.getAddress(), ethers.parseEther("20"));
+      await stakedAsset
+        .connect(alice)
+        .approve(liquidity.getAddress(), ethers.MaxUint256);
+
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        1
+      );
+      await liquidity.add(
+        stakedAsset.getAddress(),
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        true
+      );
+
+      await expect(
+        await liquidity.connect(alice).deposit(0, ethers.parseEther("1"))
+      ).to.emit(liquidity, "Deposit");
+
+      await time.increase(60 * 60 * 24); // 1 day
+
+      let pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+
+      let expected = (
+        ((1 / (60 * 60 * 24 * 365)) * (60 * 60 * 24) * 1) /
+        1
+      ).toFixed(8);
+
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await liquidity.connect(alice).withdraw(0, ethers.parseEther("1"));
+      let rewardBalance = ethers.formatEther(
+        await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+      );
+
+      expect(+rewardBalance).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+rewardBalance).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await time.increase(60 * 60 * 24 * 2); // 2 day
+
+      await expect(
+        await liquidity.connect(alice).deposit(0, ethers.parseEther("9"))
+      ).to.emit(liquidity, "Deposit");
+
+      await time.increase(60 * 60 * 24 * 30); // 30 day
+
+      pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+
+      expected = (
+        ((9 / (60 * 60 * 24 * 365)) * (60 * 60 * 24 * 30) * 1) /
+        1
+      ).toFixed(8);
+
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await liquidity.connect(alice).withdraw(0, ethers.parseEther("9"));
+      rewardBalance = ethers.formatEther(
+        await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+      );
+
+      expect(+rewardBalance).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+rewardBalance).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await time.increase(60 * 60 * 24); // 1 day
+
+      // clear reward balaance
+      await tokenRewardOneOvaReferral
+        .connect(alice)
+        .transfer(
+          await owner.getAddress(),
+          await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+        );
+
+      await liquidity.connect(alice).deposit(0, ethers.parseEther("10"));
+
+      await time.increase(60 * 60 * 24 * 30); // 30 days
+
+      pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+      expected = (
+        ((10 / (60 * 60 * 24 * 365)) * (60 * 60 * 24 * 30) * 1) /
+        1
+      ).toFixed(8);
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await liquidity.connect(alice).deposit(0, ethers.parseEther("10"));
+
+      // harvest on deposit
+      rewardBalance = ethers.formatEther(
+        await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+      );
+      expect(+rewardBalance).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+rewardBalance).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await time.increase(60 * 60 * 24 * 30); // 30 days
+
+      pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+      expected = (
+        ((20 / (60 * 60 * 24 * 365)) * (60 * 60 * 24 * 30) * 1) /
+        1
+      ).toFixed(8);
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+    });
+
+    it("Rewards for staked liquidity / 1000", async function () {
+      const { liquidity, stakedAsset, tokenRewardOneOvaReferral, alice } =
+        await loadFixture(deployFixture);
+
+      await stakedAsset.transfer(alice.getAddress(), ethers.parseEther("10"));
+      await stakedAsset
+        .connect(alice)
+        .approve(liquidity.getAddress(), ethers.MaxUint256);
+
+      await liquidity.setRewardForStakedAssets(
+        tokenRewardOneOvaReferral.getAddress(),
+        1000000,
+        1000000000
+      );
+      await liquidity.add(
+        stakedAsset.getAddress(),
+        tokenRewardOneOvaReferral.getAddress(),
+        1,
+        true
+      );
+
+      await expect(
+        await liquidity.connect(alice).deposit(0, ethers.parseEther("1"))
+      ).to.emit(liquidity, "Deposit");
+
+      await time.increase(60 * 60 * 24); // 1 day
+
+      let pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+
+      let expected = (
+        ((1 / (60 * 60 * 24 * 365)) * (60 * 60 * 24) * 1) /
+        1000
+      ).toFixed(8);
+
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await liquidity.connect(alice).withdraw(0, ethers.parseEther("1"));
+      let rewardBalance = ethers.formatEther(
+        await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+      );
+
+      expect(+rewardBalance).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+rewardBalance).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await time.increase(60 * 60 * 24 * 2); // 2 day
+
+      await expect(
+        await liquidity.connect(alice).deposit(0, ethers.parseEther("9"))
+      ).to.emit(liquidity, "Deposit");
+
+      await time.increase(60 * 60 * 24 * 30); // 1 day
+
+      pending = ethers.formatEther(
+        await liquidity.pendingReward(0, await alice.getAddress())
+      );
+
+      expected = (
+        ((9 / (60 * 60 * 24 * 365)) * (60 * 60 * 24 * 30) * 1) /
+        1000
+      ).toFixed(8);
+
+      expect(+pending).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+pending).to.be.lessThanOrEqual(+expected * 1.05);
+
+      await liquidity.connect(alice).withdraw(0, ethers.parseEther("9"));
+      rewardBalance = ethers.formatEther(
+        await tokenRewardOneOvaReferral.balanceOf(await alice.getAddress())
+      );
+
+      expect(+rewardBalance).to.be.greaterThanOrEqual(+expected * 0.95);
+      expect(+rewardBalance).to.be.lessThanOrEqual(+expected * 1.05);
+    });
+  });
+});
