57f2d53f678ed7caad8397279c7a6581b28cfe8c
Hephaestus0x <0xober@proton.me>
2025-02-07 17:41:19 +0100

    Updated block

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -77,7 +77,7 @@ const config: HardhatUserConfig = {
       forking: {
         url: PRIVATE_ETH_RPC_PREFIX + process.env.ALCHEMY_KEY!,
         enabled: true,
-        blockNumber: 21795543,
+        blockNumber: 21795000,
       },
       accounts: testAccounts,
       allowUnlimitedContractSize: true,
