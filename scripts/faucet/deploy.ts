b9c5e75047c72e902905b998a23233feaaa6e517
Hephaestus0x <0xober@proton.me>
2025-08-18 16:31:05 +0200

    Faucet deployment script

--- scripts/faucet/deploy.ts
+++ scripts/faucet/deploy.ts
@@ -1,8 +1,6 @@
 import { deploy_SepoliaFaucet } from "../functions";
 
 const USDT = "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0";
-const OverlayerWrap_USDT = "0x99116761257760775aa535C120284eFaEfe28467";
+const OverlayerWrap_USDT = "0x1Ac7E198685e53cCc3599e1656E48Dd7E278EbbE";
 
-deploy_SepoliaFaucet(USDT, OverlayerWrap_USDT).catch(
-  (e) => console.error(e)
-);
+deploy_SepoliaFaucet(USDT, OverlayerWrap_USDT).catch((e) => console.error(e));
