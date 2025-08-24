95aa7f6736ca8fe0ff2e1f9d68c148184f00708f
Hephaestus0x <0xober@proton.me>
2025-08-24 14:15:19 +0200

    Compilation & tests update

--- contracts/overlayer/interfaces/IMintRedeemManagerEvents.sol
+++ contracts/overlayer/interfaces/IMintRedeemManagerEvents.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 /* solhint-disable var-name-mixedcase  */
 /* solhint-disable func-param-name-mixedcase  */
--- contracts/overlayerbacking/AaveHandler.sol
+++ contracts/overlayerbacking/AaveHandler.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
