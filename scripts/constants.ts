9f682bb278ef7607b2789b46d864e585cf16c7df
Hephaestus0x <0xober@proton.me>
2025-11-30 11:58:59 +0100

    EURS support

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,1 +1,13 @@
 export const HARDHAT_CHAIN_ID = 31337;
+
+export const ETH_MAINNET_TOKEN_DECIMALS: { [tokenName: string]: number } = {
+  USDC: 6,
+  USDT: 6,
+  EURS: 2
+};
+
+export const SEPOLIA_TOKEN_DECIMALS: { [tokenName: string]: number } = {
+  USDC: 6,
+  USDT: 6,
+  EURS: 2
+};
